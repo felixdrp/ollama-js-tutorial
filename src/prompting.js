688ae67a57c1b6d086ed9a30555533d5b10118a9
felixdrp <felixdrp@gmail.com>
2024-08-12 18:58:32 +0100

    refactor: Use new package @langchain/ollama
    Now using ollama new method invoke that replaces the method call.

--- src/prompting.js
+++ src/prompting.js
@@ -1,16 +1,14 @@
 // *** Ollama Query a model using a prompt.
-import { Ollama } from "langchain/llms/ollama";
+import { Ollama } from "@langchain/ollama";
 
 const prompt = `why is the sky blue?`;
 
 // Select a model from Ollama
 const ollama = new Ollama({
   baseUrl: "http://localhost:11434",
-  model: "llama3",
-  // model: "llama2",
-  // model: "gemma",
-  // model: "dolphincoder",
+  model: "gemma2:2b",
+  // model: "llama3.1:latest",
 });
 
-const answer = await ollama.call(prompt);
+const answer = await ollama.invoke(prompt);
 console.log(answer);
