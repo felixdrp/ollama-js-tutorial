ef0b172d1968d558db6d411ba4c319e212727420
felixdrp <felixdrp@gmail.com>
2024-08-12 18:56:01 +0100

    refactor: Use new packages
    - Use the new CharacterTextSplitter replacing RecursiveCharacterTextSplitter
    - Use the new createRetrievalChain replacing RetrievalQAChain.

--- src/web-retrieval.js
+++ src/web-retrieval.js
@@ -1,20 +1,25 @@
 // *** Using Ollama to Query to a Web site URL
 
-import { Ollama } from "@langchain/community/llms/ollama";
-import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import { Ollama } from "@langchain/ollama";
+// Used to download a web site.
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
 
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+// import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+import { CharacterTextSplitter } from "@langchain/textsplitters";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
+// Used to generate embeddings for MemoryVectorStore
 import "@tensorflow/tfjs-node";
 import { TensorFlowEmbeddings } from "@langchain/community/embeddings/tensorflow";
 
-import { RetrievalQAChain } from "langchain/chains";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 
 const url = 'https://blogs.nvidia.com/blog/what-is-retrieval-augmented-generation/'
 const query = "Make a list of the key points of RAG"
 
 // Select a model from Ollama
-const ollama = new Ollama({
+const llm = new Ollama({
   baseUrl: "http://localhost:11434",
   // model: "llama3.1:latest",
   // model: "llama3",
@@ -27,7 +32,7 @@ const loader = new CheerioWebBaseLoader(url);
 const data = await loader.load();
 
 // Split the text into 500 character chunks. And overlap each chunk by 20 characters
-const textSplitter = new RecursiveCharacterTextSplitter({
+const textSplitter = new CharacterTextSplitter({
     // Try different sizes of chunk that better suit your model
     // chunkSize: 500,
     // chunkOverlap: 20
@@ -41,10 +46,30 @@ console.log(splitDocs)
 // Then use the TensorFlow Embedding to store these chunks in the datastore
 // const vectorStore = await MemoryVectorStore.fromDocuments(data[0].pageContent, new TensorFlowEmbeddings());
 const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, new TensorFlowEmbeddings());
-let a = new TensorFlowEmbeddings()
+// let a = new TensorFlowEmbeddings()
 
 const retriever = vectorStore.asRetriever();
-const chain = RetrievalQAChain.fromLLM(ollama, retriever);
-const result = await chain.call({query: query});
-console.log(result.text)
-console.log(await ollama.call(result.text + ` ASK YOUR QUESTION HERE `))
+
+const systemTemplate = [
+  `You are an assistant for question-answering tasks. `,
+  `Use the following pieces of retrieved context to answer `,
+  `the question. If you don't know the answer, say that you `,
+  `don't know. Use three sentences maximum and keep the `,
+  `answer concise.`,
+  `\n\n`,
+  `{context}`,
+].join("");
+
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", systemTemplate],
+  ["human", "{input}"],
+]);
+
+const questionAnswerChain = await createStuffDocumentsChain({ llm, prompt });
+const ragChain = await createRetrievalChain({
+  retriever,
+  combineDocsChain: questionAnswerChain,
+});
+
+const result = await ragChain.invoke({input: query});
+console.log(result)
