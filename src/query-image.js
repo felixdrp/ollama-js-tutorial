4fdb5864f0aad23162cc4879b3dd7fc994aaeb49
felixdrp <felixdrp@gmail.com>
2024-11-14 16:55:56 +0000

    refactor: Added ollama host config with env var OLLAMA_URL

--- src/query-image.js
+++ src/query-image.js
@@ -1,8 +1,15 @@
-// Run: bun query-image.js <IMAGE_PATH>
-// bun query-image.js ./media/576px-Pennywell_Farm_is_home_to_some_beautiful_Highland_Cows.jpg
+// Run: bun src/query-image.js <IMAGE_PATH>
+// bun src/query-image.js ./media/576px-Pennywell_Farm_is_home_to_some_beautiful_Highland_Cows.jpg
 
 import { parseArgs } from "util";
-import ollama from 'ollama'
+import { Ollama } from "ollama";
+
+const ollama = new Ollama({
+  host: process.env.OLLAMA_URL || "http://localhost:11434", // Default value
+  headers: {
+    API_KEY: process.env.API_KEY || "guest",
+  },
+});
 
 const { values, positionals } = parseArgs({
   args: Bun.argv,
@@ -15,12 +22,13 @@ const { values, positionals } = parseArgs({
 // console.log(positionals);
 
 const image = Bun.file(positionals[2]);
-const imageBytes = await image.bytes()
+const imageBytes = await image.bytes();
 // const imageEncodedBase64 = new Buffer(imageBytes).toString('base64')
 // console.log(new Buffer(imageBytes).toString('base64'))
 
 const model = 'minicpm-v'
-const prompt = "What is in this picture?"
+// const model = "llama3.2-vision";
+const prompt = "What is in this picture?";
 
 const response = await ollama.generate({
   model: model,
@@ -31,7 +39,6 @@ const response = await ollama.generate({
     // seed: 123
     // seed: 314
   },
-})
+});
 
-console.log(response)
-
+console.log(response);
