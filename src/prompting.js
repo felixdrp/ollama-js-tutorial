7ce63c8d04594f19d99b71ee570e4b6f92d3ab71
felixdrp <felixdrp@gmail.com>
2025-02-05 15:39:23 +0000

    refactor: now using ollama package
    - Switch from @langchain/ollama to ollama.
    - Added api key Authorization Bearer to the header.

--- src/prompting.js
+++ src/prompting.js
@@ -1,16 +1,18 @@
 // *** Ollama Query a model using a prompt.
-import { Ollama } from "@langchain/ollama";
+import { Ollama } from "ollama";
+const ollamaBaseUrl = process.env.OLLAMA_URL || "http://localhost:11434"; // Default value
+const ollama = new Ollama({
+  host: ollamaBaseUrl,
+  headers: {'Authorization': 'Bearer ' + process.env.OLLAMA_API_KEY,}
+})
 
 const prompt = `why is the sky blue?`;
 
-const ollamaBaseUrl = process.env.OLLAMA_URL || "http://localhost:11434"; // Default value
-
 // Select a model from Ollama
-const ollama = new Ollama({
-  baseUrl: ollamaBaseUrl,
+const answer = await ollama.generate({
   model: 'llama3.2:3b',
   // model: 'qwen2.5:1.5b',
+  prompt,
 });
 
-const answer = await ollama.invoke(prompt);
 console.log(answer);
